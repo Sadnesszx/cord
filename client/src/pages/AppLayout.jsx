f4e34687f580e9c0dda70f5292346568f59bf5d3
Sadnesszx <interienn@gmail.com>
2026-02-20 15:49:02 +0100

    fix useEffect import

--- client/src/pages/AppLayout.jsx
+++ client/src/pages/AppLayout.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import ChannelSidebar from '../components/layout/ChannelSidebar';
 import FriendsSidebar from '../components/layout/FriendsSidebar';
